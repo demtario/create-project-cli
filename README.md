9c4392a66b14b5b424f64232f9668645258c50e1
Artur <artik1999@wp.pl>
2019-03-17 15:10:57 +0100

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# create-project-cli
+This is a command-line tool for creating a boilerplate from [demtario/gulp-browsersync-php-template](https://github.com/demtario/gulp-browsersync-php-template)
+
+## Instalation
+For now to install you need to clone this repository and in it execute:
+```shell
+npm install -g
+```
+
+## Usage
+After installation you can run `create [package-name]` in whatever folder you want.  
+This command creates a folder with following name, automaticly clone boilerplate into it and initialize a git repo.
---
+++ index.js
@@ -0,0 +1,42 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const cmd = require('node-cmd-promise')
+const rimraf = require('rimraf')
+
+const details = {
+  name: process.argv[2] || false,
+  author: "",
+  description: "",
+  license: "MIT"
+}
+
+if(!details.name){
+  console.log(chalk.red("Package name is required!"))
+  process.exit(0)
+}
+
+console.log(chalk.yellow("Creating new project: ")+chalk.blue(details.name))
+
+const init = async () => {
+  await cmd('git clone https://github.com/demtario/gulp-browsersync-php-template.git '+details.name)
+  rimraf.sync(`./${details.name}/.git`)
+  await cmd(`git init ${details.name}`)
+  
+  fs.readFile(`./${details.name}/package.json`, (err, data) => {
+    if(err) throw err
+
+    const json = JSON.parse(data)
+    json.name = details.name
+    json.author = details.author || ""
+    json.description = details.description || ""
+    json.license = details.license || "MIT"
+
+    fs.writeFile(`./${details.name}/package.json`, JSON.stringify(json, null, 2), (err) => {
+      if(err) throw err
+
+      console.log(chalk.green('Project created successfully!'))
+    })
+  })
+}
+
+init()
